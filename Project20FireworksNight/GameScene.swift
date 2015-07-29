f9449f6ff86654f2e6d1000da9ce7166c02c0915
henry22 <henry811010@gmail.com>
2015-07-29 19:58:04 +0800

    createFirework() method, accept three parameters: the X movement speed of the firework, plus X and Y positions for creation.

--- Project20FireworksNight/GameScene.swift
+++ Project20FireworksNight/GameScene.swift
@@ -37,6 +37,52 @@ class GameScene: SKScene {
         gameTimer = NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: "launchFireworks", userInfo: nil, repeats: true)
     }
     
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        
+        //Create an SKNode that will act as the firework container, and place it at the X/Y position that was specified
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        //Create a rocket sprite node, give it the name "firework" so we know that it's the important thing, then add it to the container node
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        //Give the firework sprite node one of three random colors: cyan, green or red
+        switch RandomInt(min: 0, max: 2) {
+        case 0:
+            firework.color = UIColor.cyanColor()
+            firework.colorBlendFactor = 1
+        case 1:
+            firework.color = UIColor.greenColor()
+            firework.colorBlendFactor = 1
+        case 2:
+            firework.color = UIColor.redColor()
+            firework.colorBlendFactor = 1
+        default:
+            break
+        }
+        
+        //Create a UIBezierPath that will represent the movement of the firework
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 0, y: 0))
+        path.addLineToPoint(CGPoint(x: xMovement, y: 1000))
+        
+        //Tell the container node to follow that path, turning itself as needed
+        let move = SKAction.followPath(path.CGPath, asOffset: true, orientToPath: true, speed: 200)
+        node.runAction(move)
+        
+        //Create particles behind the rocket to make it look like the fireworks are lit
+        let particlePath = NSBundle.mainBundle().pathForResource("fuse", ofType: "sks")
+        let emitter = NSKeyedUnarchiver.unarchiveObjectWithFile(particlePath!) as! SKEmitterNode
+        emitter.position = CGPoint(x: 0, y: -22)
+        node.addChild(emitter)
+        
+        //Add the firework to our fireworks array and also to the scene
+        fireworks.append(node)
+        addChild(node)
+    }
+    
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
     }
